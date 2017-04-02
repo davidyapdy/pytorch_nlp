2605412c7d744dc19167b001f8a6d1d328a708ec
David <davidyap93@gmail.com>
2017-04-02 22:00:01 +0800

    TODO

---
+++ TODO.py
@@ -0,0 +1,41 @@
+# Topics to cover
+
+"""
+TODO
+
+1. Tensors
+    - Creating Tensors
+    - Operations with Tensors
+    - Reshaping Tensors
+
+2. Computation Graphs and Automatic Differentiation
+
+3. Deep Learning Building Blocks: Affine maps, non-linearities and objectives
+    - Affine Maps
+    - Non-Linearities
+    - Softmax and Probabilities
+    - Objective Functions
+
+4. Optimization and Training
+
+5. Creating Network Components in Pytorch
+    - Logistic Regression Bag-of-Words classifier
+
+6. Word Embeddings: Encoding Lexical Semantics
+    - Getting Dense Word Embeddings
+    - Word Embeddings in Pytorch
+    - N-Gram Language Modeling
+    - Computing Word Embeddings: Continuous Bag-of-Words
+
+7. Sequence Models and Long-Short Term Memory Networks (LSTMN)
+    - LSTM's in Pytorch
+    - An LSTM for Part-of-Speech Tagging
+    - Augmenting the LSTM part-of-speech tagger with character-level features
+
+8. Advanced: Dynamic Toolkits, Dynamic Programming, and the BiLSTM-CRF
+    - Dyanmic versus Static Deep Learning Toolkits
+    - Bi-LSTM Conditional Random Field Discussion
+    - The Forward Algorithm in Log-Space and the Log-Sum-Exp Trick
+    - Bidirectional LSTM Conditional Random Field for Named-Entity Recognition
+
+"""
